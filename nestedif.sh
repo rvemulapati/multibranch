3a8aa1698a8275cc0a88d20b77b4f8516f463d5d
Rohini Vemulapati <rohinivemulapati@rohinis-air.lan>
2021-01-13 15:16:31 -0500

    first commit

---
+++ nestedif.sh
@@ -0,0 +1,12 @@
+int1=$1
+int2=$2
+if [ $int1 -eq $int2 ]; then
+	echo "both are equal"
+else 
+	echo "both are not equal"
+		if [ $int1 -lt $int2 ]; then
+		echo "int1 lesser than int2"
+			else 
+			echo "values are not valid "
+fi
+fi
